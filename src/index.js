72dea13c7f13207902a39f016dcb507abe3df0b4
Renato Loureiro <renato92.hora@gmail.com>
2023-02-12 22:03:11 -0300

    dasdsa

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
-
 app.get('/talker', async (_req, res) => {
   const data = await readWriteFile();
   if (data) {
@@ -36,6 +32,24 @@ app.get('/talker/:id', async (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+  const validated = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!validated.test(email)) { 
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   const data = token();
-  res.status(200).json({ data });
-  });
+  return res.status(200).json({ data });
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
+ });
