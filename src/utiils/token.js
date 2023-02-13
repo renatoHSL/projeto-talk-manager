ecd407ee47e4bd94513d55ad622bcff83df548db
Renato Loureiro <renato92.hora@gmail.com>
2023-02-12 21:54:36 -0300

    dsada

--- src/utiils/token.js
+++ src/utiils/token.js
@@ -1,6 +1,7 @@
 const token = () => {
-    const random = Math.random().toString(16).substring(2) + Math.random().toString(16);
-    return random;
+    const number = Math.random().toString(16).substring(2) + Math.random().toString(16).substr(2);
+    const randomNumb = number.substring(0, 16);
+    return randomNumb;
 };
 
 module.exports = token;
